4e77959f6be7e4dfc0f4e9698c8dd49644964969
Borda <maximiliano.borda@capgemini.com>
2017-04-24 22:30:08 -0300

    modificaciones distintas formas de conectarse

--- Clase05MYsql/index.php
+++ Clase05MYsql/index.php
@@ -1,24 +1,74 @@
 <?php
 
+    include_once("baseDatos.php");
+    
+    
 //realizar un ABM con listado. con esta informacion , se debe de llegar a entender como modificar (traer-mostrar-guardar)
-
 $id = 101;
-$conexion = mysqli_connect("localhost","root","","utn"); //el password se deja vacio porque es una prueba local (servidor,root,pass ,base) //http://php.net/manual/es/class.mysqli.php
 
-$textoconsulta = "select * from proveedores where numero=" . $id;
-var_dump($textoconsulta);
+//ESTILO POR PROCEDIMIENTO.
+//mysqli_connect es una extension o alias del constructor mysqli::__construct()
+//$conexion = mysqli_connect("localhost","root","","utn"); //el password se deja vacio porque es una prueba local (servidor,root,pass ,base) //http://php.net/manual/es/class.mysqli.php
+//ESTILO POR OBJETO.
+
+$conexion = new mysqli("localhost", "root", "", "utn");
+
+  echo "<BR>*******************************";
+  var_dump($conexion) ;
+  echo "***********************************<BR>";
+
+if (!$conexion) {
+    echo "Error: No se pudo conectar a MySQL." . PHP_EOL;
+    echo "errno de depuración: " . mysqli_connect_errno() . PHP_EOL;
+    echo "error de depuración: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+}else{
+    echo "Éxito: Se realizó una conexión apropiada a MySQL!" . PHP_EOL;
+    echo "Información del host: " . mysqli_get_host_info($enlace) . PHP_EOL;
+}
 
 
-$consulta = mysqli_query($conexion, $textoconsulta  ) ;  //http://php.net/manual/es/mysqli.query.php
+/*
+$_objConexion = new basedatos("localhost", "root", "", "utn");
+$_objConexion->conectarse();
+*/
+
+//$textoconsulta = "select * from proveedores where numero=" . $id;
+$textoconsulta = "select * from proveedores " ;
+//var_dump($textoconsulta);
+
+//ESTILO POR PROCEDIMIENTO.
+//$consulta = mysqli_query($conexion, $textoconsulta  ) ;  //http://php.net/manual/es/mysqli.query.php
 //$consulta = mysqli_query($conexion,"select * from proveedores " ) ;
 
-var_dump($consulta);
+/**/
+//ESTILO POR OBJETO.
+if (($consulta = $conexion->query($textoconsulta))) {
+    
+    /* obtener un array asociativo */
+    while ($fila = $consulta->fetch_assoc()) {
+        //printf ("%s (%s)\n", $fila["nombre"], $fila["domicilio"] );
+        echo "<br>" . $fila["nombre"] . "<br>";
+        echo $fila["domicilio"] . "<br>";
+        echo $fila["localidad"] . "<br>";
+    }
+
+    /* liberar el conjunto de resultados */
+    $consulta->free();
+
+}else{
+    echo "Falló la obtención: (" . $conexion->errno . ") " . $conexion->error;
+}
+/**/
+
+
+//var_dump($consulta);
 echo "<br>";
 
 
-$filas = mysqli_fetch_object($consulta);
+//$filas = mysqli_fetch_object($consulta);
 
-var_dump($filas);
+//var_dump($filas);
 
 
 ?>
